f957265420ad2b18e4494eb4f63f1187d8ce34d1
HauHa <hau.ha@aem.com.sg>
2023-06-27 09:58:37 +0300

    new update info 4/2023

--- src/Component/Resume/ResumeApi.js
+++ src/Component/Resume/ResumeApi.js
@@ -2,25 +2,53 @@ const ResumeApi = [
   {
     id: 1,
     category: "education",
-    year: "Tampere University Applied of Science (TAMK) (2015-2020)",
-    title: "Energy And Environment Engineer   ",
+    year: "Helsinki Business College,HELSINKI, FINLAND   (2023)",
+    title: "Full Stack Web Developer program",
+    desc: "VOCATIONAL DEGREE IN INFORMATION & COMMUNICATIONS TECHNOLOGY",
+  },
+  {
+    id: 2,
+    category: "education",
+    year: "Tampere University Applied of Science (TAMK), TAMPERE, FINLAND (2015-2020)",
+    title: "Energy And Environment Engineer",
     desc: "Bachelor of Engineer in Energy and Environment Engineer. Major in Circular Economy",
   },
-
+  {
+    id: 3,
+    category: "experience",
+    year: "Commu App, TAMPERE, FINLAND (4/2023 - present)",
+    title: "Software developer",
+    desc: "• Designing, developing, and maintaining software applications using C# and VB .NET.\n• Collaborating with cross-functional teams to understand project requirements and deliver robust solutions.\n• Utilizing XAML to create intuitive and visually appealing user interfaces.\n• Developing GUI components and implementing responsive designs for enhanced user experience.\n• Conducting thorough testing and debugging to ensure high-quality code.\n• Conducting thorough testing and debugging to ensure high-quality code."
+  },
   {
     id: 4,
     category: "experience",
-    year: "KT Advertising and Events company, VIETNAM (2/2013 - 12/2014) ",
-    title: "Production manager",
-    desc: "Organization and management of the group such as lighting, stage, post-production … to create a beautiful and monumental event",
+    year: "Integrify, HELSINKI, FINLAND (11/2022 - 4/2023)",
+    title: "Full-stack developer",
+    desc: "Focusing on Frontend development with TypeScript, React.js, and Redux in addition to HTML, CSS, and SASS.\n\n• Emphasis on the Backend with C#, ASP.NET, Databases with MongoDB and SQL Server, and Testing through Unit Testing.\n\n• DevOps practices with CLI Scripting, Docker, and GitHub CI/CD.\n\n• Cloud computing with Microsoft Azure",
   },
   {
     id: 5,
     category: "experience",
-    year: "NEKOPARTY NFT , VIETNAM (06/2021 - 12/2021) ",
-    title: "Front-end Web Development Internship",
-    desc: "Worked with web front-end development use ReacJS,Redux toolkit .Converting designs from Photoshop to HTML, CSS and JS.Evaluating alternative implementation options",
+    year: "Commu App, TAMPERE, FINLAND (8/2022 - 3/2023)",
+    title: "Mobile developer",
+    desc: "• As a mobile developer, used React Native to build an app for a marketplace that makes it easier for users to ask for and recieve help.\n\n• The frontend technologies used were React Native and Typescript, while the backend technologies used were PHP, Laravel, and PostgreSQL",
   },
+  {
+    id: 6,
+    category: "experience",
+    year: "NEKOPARTY NFT, VIETNAM (06/2021 - 12/2021)",
+    title: "Front-end Web Development Internship",
+    desc: "Worked with web front-end development using ReactJS, Redux toolkit. Converting designs from Photoshop to HTML, CSS, and JS. Evaluating alternative implementation options",
+  },
+  {
+    id: 7,
+    category: "experience",
+    year: "KT Advertising and Events company, VIETNAM (2/2013 - 12/2014)",
+    title: "Production manager",
+    desc: "Organization and management of the group such as lighting, stage, post-production… to create a beautiful and monumental event",
+  }
+ 
 ];
 
 export default ResumeApi;
