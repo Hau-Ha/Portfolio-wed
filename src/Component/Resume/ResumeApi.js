5868763f52f0ddc04845e314990b751ea9deb9b7
Hau Ha <haduyhau1995@gmail.com>
2024-02-12 12:20:46 +0200

    new update 1

--- src/Component/Resume/ResumeApi.js
+++ src/Component/Resume/ResumeApi.js
@@ -21,28 +21,28 @@ const ResumeApi = [
     desc: "Bachelor of Engineer in Energy and Environment Engineer. Major in Circular Economy",
   },
   {
-    id: 3,
+    id: 4,
     category: "experience",
     year: "AEM Afore oy ,Turku ,Finland (4/2023 - present)",
     title: "Software developer",
     desc: "• Designing, developing, and maintaining software applications using C# and VB .NET.\n• Collaborating with cross-functional teams to understand project requirements and deliver robust solutions.\n• Utilizing XAML to create intuitive and visually appealing user interfaces.\n• Developing GUI components and implementing responsive designs for enhanced user experience.\n• Conducting thorough testing and debugging to ensure high-quality code.\n• Conducting thorough testing and debugging to ensure high-quality code."
   },
   {
-    id: 3,
+    id: 5,
     category: "experience",
     year: "Power international as Oy, FINLAND (4/2023 - present)",
     title: "Back-end Developer trainee",
     desc: "•  Focused on the design, development, and maintenance of software applications using C#.\n• • Addressed bugs and developed new features for the Power store across the Nordic region.\n•  Directly collaborated on the e-commerce website, contributing to various aspects of online retailing."
   },
   {
-    id: 4,
+    id: 6,
     category: "experience",
     year: "Integrify, HELSINKI, FINLAND (11/2022 - 1/2023)",
     title: "Full-stack developer",
     desc: "Focusing on Frontend development with TypeScript, React.js, and Redux in addition to HTML, CSS, and SASS.\n\n• Emphasis on the Backend with C#, ASP.NET, Databases with MongoDB and SQL Server, and Testing through Unit Testing.\n\n• DevOps practices with CLI Scripting, Docker, and GitHub CI/CD.\n\n• Cloud computing with Microsoft Azure",
   },
   {
-    id: 5,
+    id: 7,
     category: "experience",
     year: "Commu App, TAMPERE, FINLAND (8/2022 - 12/2022)",
     title: "Mobile developer trainee",
