3f2e699ccd2ddaba77ce464af7a37926252434d5
HauHa-Healthfox <hau.ha@healthfox.fi>
2026-02-01 02:10:21 +0200

    fix js fuction

--- src/Component/Resume/Resume.js
+++ src/Component/Resume/Resume.js
@@ -4,66 +4,65 @@ import ResumeApi from "./ResumeApi";
 import Card from "./Card";
 
 const Resume = () => {
+  const educationData = ResumeApi.filter((val) => val.category === "education");
+
+  const experienceData = ResumeApi.filter(
+    (val) => val.category === "experience"
+  );
+
   return (
-    <>
-      <section className="Resume" id="resume">
-        <div className="container top">
-          <div className="heading text-center">
-            <h1>My Resume</h1>
-            <button className="homebtn">
-              <a rel="noreferrer" target="_blank" href="./image/CV.pdf">
-                My CV
-              </a>
-            </button>
+    <section className="Resume" id="resume">
+      <div className="container top">
+        <div className="heading text-center">
+          <h1>My Resume</h1>
+          <button className="homebtn">
+            <a rel="noreferrer" target="_blank" href="./image/CV.pdf">
+              My CV
+            </a>
+          </button>
+        </div>
+
+        <div className="content-section mtop d_flex">
+          {/* EDUCATION */}
+          <div className="left">
+            <div className="heading">
+              <h1>Education</h1>
+            </div>
+
+            <div className="content">
+              {educationData.map((val, id) => (
+                <Card
+                  key={id}
+                  title={val.title}
+                  year={val.year}
+                  rate={val.rate}
+                  desc={val.desc}
+                />
+              ))}
+            </div>
           </div>
 
-          <div className="content-section mtop d_flex">
-            <div className="left">
-              <div className="heading">
-                <h1>Education</h1>
-              </div>
-
-              <div className="content">
-                {ResumeApi.map((val, id) => {
-                  if (val.category === "education") {
-                    return (
-                      <Card
-                        key={id}
-                        title={val.title}
-                        year={val.year}
-                        rate={val.rate}
-                        desc={val.desc}
-                      />
-                    );
-                  }
-                })}
-              </div>
+          {/* EXPERIENCE */}
+          <div className="left">
+            <div className="heading">
+              <h1>Job Experience</h1>
             </div>
-            <div className="left">
-              <div className="heading">
-                <h1>Job Experience</h1>
-              </div>
 
-              <div className="content">
-                {ResumeApi.map((val, index) => {
-                  if (val.category === "experience") {
-                    return (
-                      <Card
-                        key={index}
-                        title={val.title}
-                        year={val.year}
-                        rate={val.rate}
-                        desc={val.desc}
-                      />
-                    );
-                  }
-                })}
-              </div>
+            <div className="content">
+              {experienceData.map((val, index) => (
+                <Card
+                  key={index}
+                  title={val.title}
+                  year={val.year}
+                  rate={val.rate}
+                  desc={val.desc}
+                />
+              ))}
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
